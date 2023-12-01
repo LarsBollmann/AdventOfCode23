16e57e056d78bd424cac01e9d40fdbb77e2b2164
Lars Bollmann <lars.bollmann@tu-dortmund.de>
2023-12-01 08:27:08 +0100

    1.1

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,17 +1,24 @@
-use aoc23::input;
+use aoc23::input::get_input;
 
-pub fn part_one(input_string: String) -> u64 {
-    1
+pub fn part_one(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let chars: Vec<u32> = line.chars().filter_map(|c| c.to_digit(10)).collect();
+            chars[0] * 10
+                + chars[chars.len() - 1]
+        })
+        .sum()
 }
 
-pub fn part_two(input_string: String) -> u64 {
+pub fn part_two(input: &str) -> u32 {
     1
 }
 
 pub fn main() {
-    let input = input::get_input(1);
-    println!("Part one answer: {}", part_one(input.clone()));
-    println!("Part two answer: {}", part_two(input));
+    let input = get_input(1);
+    println!("Part one answer: {}", part_one(&input));
+    println!("Part two answer: {}", part_two(&input));
 }
 
 #[cfg(test)]
@@ -20,13 +27,22 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let input = input::get_example(1);
-        assert_eq!(part_one(input), 1);
+        assert_eq!(
+        part_one("1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet"), 142);
     }
 
     #[test]
     fn test_part_two() {
-        let input = input::get_example(1);
-        assert_eq!(part_two(input), 1);
+        assert_eq!(part_two(
+        "two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen"), 281);
     }
 }
