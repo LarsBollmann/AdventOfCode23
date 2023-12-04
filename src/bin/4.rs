a2a5d210189458831491f2315d55a09a052ebfe2
Lars Bollmann <lars.bollmann@tu-dortmund.de>
2023-12-04 22:38:20 +0100

    Day 4 task two

--- src/bin/4.rs
+++ src/bin/4.rs
@@ -1,31 +1,68 @@
 use aoc23::input::get_input;
 
+pub fn get_n_winning(winning: &[u32], own: &[u32]) -> usize {
+    winning
+        .iter()
+        .filter(|winning_number| own.contains(winning_number))
+        .count()
+}
+
 pub fn parse_numbers(numbers: &str) -> Vec<u32> {
-    numbers.split_whitespace().map(|n| n.parse().unwrap()).collect()
+    numbers
+        .split_whitespace()
+        .map(|number| number.parse().unwrap())
+        .collect()
 }
 
 pub fn parse_line(input: &str) -> (Vec<u32>, Vec<u32>) {
     let (_, input) = input.split_once(": ").unwrap();
-    let (winning, own) = input.split_once('|').unwrap();
-    let winning = parse_numbers(winning);
-    let own = parse_numbers(own);
-    (winning, own)
+    let (winning_str, own_str) = input.split_once('|').unwrap();
+
+    (parse_numbers(winning_str), parse_numbers(own_str))
 }
 
 pub fn part_one(input: &str) -> u32 {
-    input.lines().map(parse_line).map(|(winning, own)| {
-        let n_wins = winning.iter().filter(|n| own.contains(n)).count();
-        println!("{:?} {:?} {}", winning, own, n_wins);
-       match n_wins {
-            0 => 0,
-            n => 1 << (n - 1),
-       }
-    }).sum()
+    input
+        .lines()
+        .map(parse_line)
+        .map(|(winning, own)| {
+            let n_wins = get_n_winning(&winning, &own);
+
+            match n_wins {
+                0 => 0,
+                n => 1 << (n - 1),
+            }
+        })
+        .sum()
+}
+
+#[derive(Debug)]
+struct Card {
+    count: u32,
+    winning: Vec<u32>,
+    own: Vec<u32>,
 }
 
 pub fn part_two(input: &str) -> u32 {
+    let mut cards = input
+        .lines()
+        .map(parse_line)
+        .map(|(winning, own)| Card {
+            count: 1,
+            winning,
+            own,
+        })
+        .collect::<Vec<_>>();
 
-    1
+    for i in 0..cards.len() {
+        let n_wins = get_n_winning(&cards[i].winning, &cards[i].own);
+
+        for j in i + 1..i + 1 + n_wins {
+            cards[j].count += cards[i].count;
+        }
+    }
+
+    cards.iter().map(|c| c.count).sum()
 }
 
 pub fn main() {
@@ -48,6 +85,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = get_example(4);
-        assert_eq!(part_two(&input), 467835);
+        assert_eq!(part_two(&input), 30);
     }
 }
